3fa4be9d24e4eff03c0f193a2f1058d3f39e588a
Gordon Parikh <parik018@umn.edu>
2017-01-13 15:32:42 -0600

    Added option to specify config section

--- imageselector.py
+++ imageselector.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Program for selecting points and regions on an image for use in computer vision applications.")
     parser.add_argument('imageFilename', help="Name of the image file to draw on.")
     parser.add_argument('-f', dest='configFilename', help="Name of file containing point/region locations.")
+    parser.add_argument('-s', dest='configSection', help="Section of the config file containing point/region locations. Defaults to name of the image file (without the path) if not specified.")
     parser.add_argument('-pk', dest='printKeys', action='store_true', help="Print keys that are read from the video window (useful for adding shortcuts and other functionality).")
     parser.add_argument('-pm', dest='printMouseEvents', type=int, nargs='*', help="Print mouse events that are read from the video window (useful for adding other functionality). Optionally can provide a number, which signifies the minimum event flag that will be printed.")
     parser.add_argument('-r', dest='clickRadius', type=int, default=10, help="Radius of clicks on the image (in pixels).")
@@ -20,9 +21,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     imageFilename = args.imageFilename
     configFilename = args.configFilename
+    configSection = args.configSection
     
     # create the GUI object
-    gui = cvgui.cvGUI(imageFilename, configFilename, printKeys=args.printKeys, printMouseEvents=args.printMouseEvents, clickRadius=args.clickRadius)
+    gui = cvgui.cvGUI(imageFilename, configFilename=configFilename, configSection=configSection, printKeys=args.printKeys, printMouseEvents=args.printMouseEvents, clickRadius=args.clickRadius)
     
     # show the window
     if args.interactive:
