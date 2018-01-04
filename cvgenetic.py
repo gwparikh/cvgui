9fd14e6c3d5fcf3f0cd1480e566c34dd0b5e8a1e
Kah Hin Lai <laixx330@umn.edu>
2018-01-04 15:19:00 -0600

    feat(cvgenetic): cvgenetic module for genetic algorithm

--- cvgenetic.py
+++ cvgenetic.py
@@ -2,32 +2,83 @@
 
 from random import randint
     
-class population(object):
-    def __init__(self, size, CalculateFitness):
+class Population(object):
+    def __init__(self, size):
         self.size = size
-        self.CalculateFitness = CalculateFitness;
         individuals = []
-    def add(self, individual):
+        
+    def add(self, newindividual):
         if len(individuals) < size:
-            newindivudal = (individual, CalculateFitness(individual))
             individuals.append(individual)
         else:
-            individuals[self.get_least] = individual
+            least = self.get_least_index()
+            if individuals[least][1] < newindividual[1]:
+                individuals[least] = newindividual
             
+    def get_least_index(self):
+        least = 0
+        for i in range(len(individuals)):
+            if individuals[i][1] < individuals[least][1]:
+                least = i;
+        return least
     
-    def get_least(self):
-        
+    # return a list contains two individuals (best and the second best)
     def get_two_best(self):
+        if individuals[0][1] > individuals[1][1]:
+            best_two = [individuals[0], individuals[1]]
+        else:
+            best_two = [individuals[1], individuals[0]]
+        for individual in individuals:
+            if individual[1] > best_two[0][1]:
+                best_two[0] = individual
+            elif individual[1] > best_two[1][1]:
+                best_two[1] = individual
+        return best_two
 
 class CVGenetic(object):
-    def __init__(self, population_size, DataList, CalculateFitness):
+    def __init__(self, population_size, DataList, CalculateFitness, accuracy = 10):
+        population = Population(population_size)
+        self.DataList = DataList
+        self.accuracy = accuracy
         self.CalculateFitness = CalculateFitness
-        population = []
-        self.size = size
+        timer = 0
+        best = None
         
-    def selection(self):
+    def select(self):
+        best_two = population.get_two_best()
+        if best == best_two[0][0]:
+            timer += 1
+        else:
+            best = best_two[0][0]
+            timer = 0
+        return best_two[0][0], best_two[1][0]
+    
+    def crossover(self, parent1, parent2):
+        return DataList.crossover(parent1, parent2)
         
-    def crossover(self):
+    def mutation(self, offspring):
+        return DataList.mutation(offspring)
         
-    def mutation(self):
-        
+    def run(self):
+        timer = 0
+        while True:
+            best, second_best = self.select()
+            offspring1, offspring2 = self.crossover(best, second_best)
+            offspring1 = self.mutation(offspring1)
+            offspring2 = self.mutation(offspring2)
+            fitness1 = CalculateFitness(offspring1)
+            fitness2 = CalculateFitness(offspring2)
+            if fitness1 > fitness2:
+                newindividual = (offspring1, fitness1)
+            else:
+                newindividual = (offspring2, fitness2)
+            population.add(newindividual)
+            if timer == accuracy:
+                break
+        return best
+            
+            
+            
+            
+            
+            
