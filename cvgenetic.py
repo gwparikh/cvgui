58a313f53a7fde0bbe7cd1cdaf2b9431ea472f79
Kah Hin Lai <laixx330@umn.edu>
2018-01-13 16:35:19 -0600

    fix(cvgenetic.py): fix errors

--- cvgenetic.py
+++ cvgenetic.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python
-
 from random import randint
     
 class Population(object):
     def __init__(self, size):
         self.size = size
-        individuals = []
+        self.individuals = []
         
     def add(self, newindividual):
-        if len(individuals) < size:
-            individuals.append(individual)
+        if len(self.individuals) < self.size:
+            self.individuals.append(newindividual)
         else:
             least = self.get_least_index()
-            if individuals[least][1] < newindividual[1]:
-                individuals[least] = newindividual
+            if self.individuals[least][1] < newindividual[1]:
+                self.individuals[least] = newindividual
             
     def get_least_index(self):
         least = 0
-        for i in range(len(individuals)):
-            if individuals[i][1] < individuals[least][1]:
+        for i in range(len(self.individuals)):
+            if self.individuals[i][1] < self.individuals[least][1]:
                 least = i;
         return least
     
     # return a list contains two individuals (best and the second best)
     def get_two_best(self):
-        if individuals[0][1] > individuals[1][1]:
-            best_two = [individuals[0], individuals[1]]
+        if self.individuals[0][1] > self.individuals[1][1]:
+            best_two = [self.individuals[0], self.individuals[1]]
         else:
-            best_two = [individuals[1], individuals[0]]
-        for individual in individuals:
+            best_two = [self.individuals[1], self.individuals[0]]
+        for individual in self.individuals:
             if individual[1] > best_two[0][1]:
                 best_two[0] = individual
             elif individual[1] > best_two[1][1]:
@@ -37,45 +36,51 @@ class Population(object):
 
 class CVGenetic(object):
     def __init__(self, population_size, DataList, CalculateFitness, accuracy = 10):
-        population = Population(population_size)
+        self.population = Population(population_size)
         self.DataList = DataList
         self.accuracy = accuracy
+        self.best = float("-inf")
         self.CalculateFitness = CalculateFitness
-        timer = 0
-        best = None
+        for i in range(population_size):
+            individual = DataList.RandomIndividual()
+            fitness = CalculateFitness(individual)
+            if self.best < fitness:
+                self.best = fitness
+            newindividual = (individual, fitness)
+            self.population.add(newindividual)
+        self.timer = 0
         
     def select(self):
-        best_two = population.get_two_best()
-        if best == best_two[0][0]:
-            timer += 1
+        best_two = self.population.get_two_best()
+        if self.best == best_two[0][0]:
+            self.timer += 1
         else:
-            best = best_two[0][0]
-            timer = 0
+            self.best = best_two[0][0]
+            self.timer = 0
         return best_two[0][0], best_two[1][0]
     
     def crossover(self, parent1, parent2):
-        return DataList.crossover(parent1, parent2)
+        return self.DataList.crossover(parent1, parent2, randint(0, self.DataList.length()))
         
     def mutation(self, offspring):
-        return DataList.mutation(offspring)
+        return self.DataList.mutation(offspring)
         
     def run(self):
-        timer = 0
+        self.timer = 0
         while True:
             best, second_best = self.select()
             offspring1, offspring2 = self.crossover(best, second_best)
             offspring1 = self.mutation(offspring1)
             offspring2 = self.mutation(offspring2)
-            fitness1 = CalculateFitness(offspring1)
-            fitness2 = CalculateFitness(offspring2)
+            fitness1 = self.CalculateFitness(offspring1)
+            fitness2 = self.CalculateFitness(offspring2)
             if fitness1 > fitness2:
                 newindividual = (offspring1, fitness1)
             else:
                 newindividual = (offspring2, fitness2)
-            population.add(newindividual)
-            if timer == accuracy:
+            self.population.add(newindividual)
+            if self.timer == self.accuracy:
                 break
-        return best
             
             
             
