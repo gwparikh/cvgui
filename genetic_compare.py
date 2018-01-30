067259952fcf6d9f53316bd563cd5c0b18515b94
Kah Hin Lai <laixx330@umn.edu>
2018-01-29 18:00:59 -0600

    add more parameter to argparser

--- genetic_compare.py
+++ genetic_compare.py
@@ -51,8 +51,9 @@ if __name__ == '__main__' :
     parser.add_argument('-f', '--First-ID', dest ='firstID', help = "the first ID of the range of ID", required = True, type = int)
     parser.add_argument('-l', '--Last-ID', dest ='lastID', help = "the last ID of the range of ID", required = True, type = int)
     parser.add_argument('-m', '--matching-distance', dest='matchDistance', help = "matchDistance", default = 10, type = float)
-    parser.add_argument('-a', '--accuracy', dest ='accuracy', help = "accuracy parameter for genetic algorithm", type = int)
-    parser.add_argument('-p', '--num-of-parents', dest = 'num_of_parents', help = "Number of parents are selected each generation", type = int)
+    parser.add_argument('-a', '--accuracy', dest = 'accuracy', help = "accuracy parameter for genetic algorithm", type = int)
+    parser.add_argument('-p', '--population', dest = 'population', help = "population parameter for genetic algorithm", required = True, type = int)
+    parser.add_argument('-np', '--num-of-parents', dest = 'num_of_parents', help = "Number of parents that are selected each generation", type = int)
     parser.add_argument('-mota', '--print-MOTA', dest='PrintMOTA', action = 'store_true', help = "Print MOTA for each ID.")
     args = parser.parse_args()
     
@@ -85,9 +86,9 @@ if __name__ == '__main__' :
     cfg_list = cfgcomb.CVConfigList()
     cfgcomb.config_to_list(cfg_list, config)
     if args.accuracy != None:
-        GeneticCal = cvgenetic.CVGenetic(10, cfg_list, Comp.computeMOT, args.accuracy)
+        GeneticCal = cvgenetic.CVGenetic(args.population, cfg_list, Comp.computeMOT, args.accuracy)
     else:
-        GeneticCal = cvgenetic.CVGenetic(10, cfg_list, Comp.computeMOT)
+        GeneticCal = cvgenetic.CVGenetic(args.population, cfg_list, Comp.computeMOT)
     if args.num_of_parents != None:
         GeneticCal.run_thread(args.num_of_parents)
     else:
