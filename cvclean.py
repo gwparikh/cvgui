f50794de35d669250d9863ea87c7e5bb2cc98d17
Kah Hin Lai <laixx330@umn.edu>
2018-05-31 04:03:17 -0500

    update cvclean

--- cvclean.py
+++ cvclean.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-from os import path
+from os import path, remove
 from shutil import rmtree
 
 if __name__ == '__main__':
     if path.exists('sql_files'):
         rmtree('sql_files')
-        print 'deleted sql_files directory'
+        print('deleted sql_files directory')
     if path.exists('cfg_files'):
         rmtree('cfg_files')
-        print 'deleted cfg_files directory'
+        print('deleted cfg_files directory')
+    if path.exists('tracking_only.sqlite'):
+        remove('tracking_only.sqlite')
+        print('deleted tracking_only.sqlite')
